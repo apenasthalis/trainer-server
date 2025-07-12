02eda892209970192eb4afa263d6d9dbd01883c4
thalis <thalis@ipeweb.com.br>
2025-07-12 11:54:36 -0300

    inicializing api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,10 +5,15 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    // ->withRouting(
+    //     web: __DIR__.'/../routes/web.php',
+    //     commands: __DIR__.'/../routes/console.php',
+    //     health: '/up',
+    // )
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'api/',
+        // ...
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
